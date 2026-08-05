c438c310b7de9b68b8f990c4867938c6d2a67b20
Matthew Washburn <mdwashburn02@gmail.com>
2026-08-05 19:13:35 +0000

    Run tests

--- jenkins-spring-demo/src/test/java/com/example/jenkins_spring_demo/JenkinsSpringDemoApplicationTests.java
+++ jenkins-spring-demo/src/test/java/com/example/jenkins_spring_demo/JenkinsSpringDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.jenkins_spring_demo;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class JenkinsSpringDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testNull() {
+		assertNull(null);
 	}
 
 }
